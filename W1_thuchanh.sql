6d36b96a5897fe0802c0f617b70c10aca6b06571
tuyetngth2558 <tuyet.nguyenthi.17081999@gmail.com>
2025-12-09 22:36:23 +0700

    Add SQL query to rank top customers by spending
    This SQL query identifies the top spending customers in 2023, segmenting them into 'Diamond' and 'Gold' categories based on their total spending.

---
+++ W1_thuchanh.sql
@@ -0,0 +1,33 @@
+WITH customer_spending AS (
+    SELECT
+        user_id,
+        SUM(sale_price) AS total_spend -- Tính tổng giá bán
+    FROM `bigquery-public-data.thelook_ecommerce.order_items`
+    WHERE status = 'Complete'
+      AND EXTRACT (YEAR from created_at)  = 2023 -- Lọc năm 2023 
+    GROUP BY user_id
+),
+ranked_customers AS (
+    SELECT
+        u.first_name,
+        u.last_name,
+        u.country,
+        cs.total_spend,
+        CASE
+            WHEN cs.total_spend > 500 THEN 'Diamond'
+            ELSE 'Gold'
+        END AS customer_segment,
+
+        RANK() OVER (
+            PARTITION BY country 
+            ORDER BY  cs.total_spend  DESC 
+        ) AS rank_in_country
+
+    FROM customer_spending AS cs
+    JOIN `bigquery-public-data.thelook_ecommerce.users` AS u
+        ON cs.user_id = u.id 
+)
+SELECT *
+FROM ranked_customers
+WHERE rank_in_country = 1
+ORDER BY total_spend DESC; 
